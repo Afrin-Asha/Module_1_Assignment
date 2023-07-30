25ae31483d54aed149846ebb11e11725570ad493
Afrin-Asha <116973254+Afrin-Asha@users.noreply.github.com>
2023-07-30 17:41:02 +0600

    Delete Topic_2.js

--- Topic_2.js
+++
@@ -1,6 +0,0 @@
-let givenArray = prompt("Enter array values separated by commas: ");
-let array = givenArray.split(",");
-evenArray = array.filter(function(element){
-    return element % 2 == 0;
-});
-console.log("Original Array: "+array+"<br>Filtered Even Numbers: "+evenArray);
