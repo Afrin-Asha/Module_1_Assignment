b06df21e5e070d9f289e48d3caf468e1d017e573
Afrin-Asha <116973254+Afrin-Asha@users.noreply.github.com>
2023-07-30 17:38:52 +0600

    Update Topic_2.js

--- Topic_2.js
+++ Topic_2.js
@@ -3,4 +3,4 @@ let array = givenArray.split(",");
 evenArray = array.filter(function(element){
     return element % 2 == 0;
 });
-document.write("Original Array: "+array+"<br>Filtered Even Numbers: "+evenArray);
+console.log("Original Array: "+array+"<br>Filtered Even Numbers: "+evenArray);
